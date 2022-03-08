8b8b712da682a7878770460c84118d0002eda0b3
Your Name <adepennart@gmail.com>
2022-03-08 10:59:22 +0100

    script works, minor bug with scientific name in lineage output

--- taxonpathfinder.py
+++ taxonpathfinder.py
@@ -41,6 +41,9 @@ Usage:
     python mini_tax.dat homo_sapiends.json
 
 known error:
+    1. use mispelled as well
+    
+    
     1. reverse list is longer than original list
     2. may not find all species names
 
@@ -105,9 +108,10 @@ output = open(outputfile, 'w')
 for n,line in enumerate(inputted):
     #print(line)
     taxonomy.append(line.strip())
-    if re.search('^ID\s+:\s207598', line):#so weird
-        print("hey")
-        # break
+    # if re.search('^ID\s+:\s207598', line):#so weird
+    #     print("hey")
+    #     # break
+
     if re.search('RANK\s+:\sspecies', line):  # finds line if fasta header
         # print(line)
         rank_s=re.search('RANK\s+:\s(species)', line).group(1)
@@ -120,9 +124,13 @@ for n,line in enumerate(inputted):
                 rank_s=re.search('SCIENTIFIC NAME\s+:\s([A-Za-z\d\-\[\]]+)\s([A-Za-z\s\d\-\[\]]+)', line.strip()).group(1,2)        
                 species_dict[rank_s[0]]=rank_s[1] 
                 flag=False
-                if re.search('sapiens', line):
+                #may be able to make last character not be white space/however it did find homo sapiens, try and break
+                #also updated to other organisms
+                #this was modified, not sure if still works (added$)
+                if re.search('SCIENTIFIC NAME\s+:\sHomo\ssapiens$', line):
                     human=re.search('(SCIENTIFIC NAME\s+:\s[A-Za-z\s\d\-\[\]]+)', line.strip()).group(1) 
                     index=n
+                    print(index)
                     print(human)
                     #break#break when find first sapiens
                 else:
@@ -136,6 +144,34 @@ for n,line in enumerate(inputted):
         pass
 
 
+'''
+[ {
+  "taxId" : "9606",
+  "scientificName" : "Homo sapiens",
+  "commonName" : "human",
+  "formalName" : "true",
+  "rank" : "species",
+  "division" : "HUM",
+  "lineage" : "Eukaryota; Metazoa; Chordata; Craniata; Vertebrata; Euteleostomi; Mammalia; Eutheria; Euarchontoglires; Primates; Haplorrhini; Catarrhini; Hominidae; Homo; ",
+  "geneticCode" : "1",
+  "mitochondrialGeneticCode" : "2",
+  "submittable" : "true"
+} ]
+'''
+
+#%%
+#temporary location
+json_dict={}
+first_MGC=True
+first_GC=True
+first_rank=True
+first_ID=True
+first_Genebank=True
+lineage=""
+one_scientific_name=True
+#temporary variable
+index=63207
+count=0
 
 #fix order of this= so it looks good
 while 1:
@@ -144,15 +180,69 @@ while 1:
         if '//' in taxonomy[index-count]:
             print(taxonomy[index-count])
             break
+        #this needs to be updated for other organisms
+        if one_scientific_name:
+            if re.search('SCIENTIFIC NAME\s+:\sHomo\ssapiens$', taxonomy[index-count]):
+                #this is repeated from the for loop through the database
+                print(taxonomy[index-count])
+                rank_s=re.search('SCIENTIFIC NAME\s+:\s([A-Za-z\d\-\[\]]+)\s([A-Za-z\s\d\-\[\]]+)', taxonomy[index-count]).group(1,2)        
+                species_dict[rank_s[0]]=rank_s[1] 
+                json_dict["scientificName"]=rank_s[0]+' '+rank_s[1]
+                lineage+=f'{rank_s[0]} {rank_s[1]}; ' 
+                count+=1
+                one_scientific_name=False
+            else:
+                print(taxonomy[index+count])
+                count+=1
+        elif first_MGC:
+            if re.search('^MGC ID\s+:\s[\d]+', taxonomy[index-count]): 
+                print(taxonomy[index-count])
+                MGC=re.search('^MGC ID\s+:\s([\d]+)', taxonomy[index-count]).group(1)
+                json_dict['mitochondrialGeneticCode']=MGC
+                first_MGC=False 
+                count+=1
+            else:
+                print(taxonomy[index-count])
+                count+=1
+        elif first_GC:
+            if re.search('^GC ID\s+:\s[\d]+', taxonomy[index-count]): 
+                print(taxonomy[index-count])
+                GC=re.search('^GC ID\s+:\s([\d]+)', taxonomy[index-count]).group(1)
+                json_dict['geneticCode']=GC
+                first_GC=False 
+                count+=1
+            else:
+                print(taxonomy[index-count])
+                count+=1
+        elif first_rank:
+            if re.search('^RANK\s+:\s[A-Za-z]+', taxonomy[index-count]): 
+                print(taxonomy[index-count])
+                rank=re.search('^RANK\s+:\s([A-Za-z]+)', taxonomy[index-count]).group(1)
+                json_dict['rank']=rank
+                first_rank=False   
+                count+=1
+            else:
+                print(taxonomy[index-count])
+                count+=1
         elif re.search('PARENT ID', taxonomy[index-count]):
             print(taxonomy[index-count])
             parent_id=taxonomy[index-count]
             count+=1
+        elif first_ID:
+            if re.search('^ID', taxonomy[index-count]):
+                print(taxonomy[index-count])
+                ID=re.search('^ID\s+:\s([\d]+)', taxonomy[index-count]).group(1)
+                json_dict['taxId']=ID
+                first_ID=False
+                count+=1
+            else:
+                print(taxonomy[index-count])
+                count+=1
         else:
             print(taxonomy[index-count])
             count+=1
     count=1
-    
+    one_scientific_name=True
     while 1:
         if '//' in taxonomy[index+count]:
             print(taxonomy[index+count])
@@ -161,100 +251,52 @@ while 1:
             print(taxonomy[index+count])
             parent_id=taxonomy[index+count]
             count+=1
+        #why is this needed
+        elif one_scientific_name:
+            if re.search('SCIENTIFIC NAME\s+:\s[A-Za-z\s\d\-\[\]]+$', taxonomy[index-count]):
+                #this is repeated from the for loop through the database
+                print(taxonomy[index-count])
+                name=re.search('SCIENTIFIC NAME\s+:\s([A-Za-z\s\d\-\[\]]+)', taxonomy[index-count]).group(1)        
+                lineage+=f'{name}; ' 
+                count+=1
+                one_scientific_name=False
+            else:
+                print(taxonomy[index+count])
+                count+=1
+        elif first_Genebank:
+            #assuming all names are just letters
+            if re.search('^GENEBANK COMMON NAME\s+:\s[A-Za-z\s]+', taxonomy[index-count]): 
+                print(taxonomy[index-count])
+                Genebank=re.search('^GENEBANK COMMON NAME\s+:\s([A-Za-z\s]+)', taxonomy[index-count]).group(1)
+                json_dict['CommonName']=Genebank
+                first_Genebank=False
+                count+=1
+            elif re.search('^COMMON NAME\s+:\s[A-Za-z\s]+', taxonomy[index-count]): 
+                print(taxonomy[index-count])
+                common=re.search('^COMMON NAME\s+:\s([A-Za-z\s]+)', taxonomy[index-count]).group(1)
+                json_dict['CommonName']=common
+                first_Genebank=False 
+                count+=1
+            else:
+                print(taxonomy[index-count])
+                count+=1
         else:
             print(taxonomy[index+count])
             count+=1
     count=0
+    one_scientific_name=True
     print(f'{parent_id} is the parent ID')
     ID=re.search('^PARENT ID\s+:\s([\d]+)', parent_id).group(1)
     ID=f'ID                        : {ID}'
-    # try:
-    #     if index=taxonomy.index(ID):
-    index=taxonomy.index(ID)
-        
+    try:
+        index=taxonomy.index(ID)
+    except ValueError:
+        print("Reached root")
+        break
+json_dict["Lineage"]=lineage
+
+print(json_dict)       
     
 
-# for line in range(1,len(taxonomy)+1):
-#     # print(taxonomy[-line])
-#     taxonomy_rev.append(taxonomy[-line])
-    
-# #print(species_dict)
-# species=species_dict['Homo']
-# genus_species=f'Homo {species}'
-# for n,line in enumerate(taxonomy_rev):
-#     if genus_species in line:
-#         parent_check=True
-#         print(line)
-#         # print(n)
-#         # index_n=n
-#         # figure out how to fix this
-#         # index=(taxonomy_rev.index(genus_species))
-#         # print(index)
-#     elif re.search('PARENT ID', line):
-#         if parent_check:
-#             print(line)
-#             parent_ID=re.search('PARENT ID\s+:\s([\d]+)', line).group(1)
-#             print(f'this is {parent_ID}')
-#             parent_check=False
-#             first=True
-#         else:
-
-#             ID_line=re.search('PARENT ID\s+:\s([\d]+)', line).group(1)
-#             # print(ID_line)
-#             # print("Entered else")
-#             pass
-#     elif re.search('^ID', line):   
-#         if first:
-#             #print(parent_ID)
-#             # if re.search(parent_ID, line):
-#             print(line)
-#             new_parent_ID=taxonomy_rev[index_n-1]
-#             print(new_parent_ID)
-#             new_parent_ID=re.search('^PARENT ID\s+:\s([\d]+)', new_parent_ID).group(1)
-#             print(new_parent_ID)
-#             first=False
-#         elif re.search(new_parent_ID, line):
-#             print(line)
-#         # if re.search(parent_ID, line) or re.search(ID_line, line):
-#         #     print(line)
-#         #     parent_ID=re.search('PARENT ID\s+:\s([\d]+)', ID_line).group(1)
-#         #     print(parent_ID)
-#         #     print(n)
-#         #     index_n=n
-#         else:
-#             # print(line)
-#             pass
-#     elif re.search('^ID\s+:\s207598', line):
-#         print("hey")
-#     else:
-#         # print(line)
-#         pass
-
-
-
-# print("__________")
-# print(taxonomy_rev[index_n])        
-# print(taxonomy_rev[index_n-1]) 
-# print(taxonomy_rev[index_n-2]) 
-# print(taxonomy_rev[index_n-3]) 
-# print(taxonomy_rev[index_n-4]) 
-# print(taxonomy_rev[index_n-5]) 
-
-
-#print(species_dict['Anolis'])
-
-    # if re.search('SCIENTIFIC NAME', line):
-    #     if 
-    
-    
-    # if re.search('^>[^?*]+$', line): #finds line if fasta header
-    #     print(line)
-    #     pass
-    # elif not re.search('^>[^?*]+$', line): #finds line if not fasta header
-    #     print(line)
-    #     pass
-    # else:
-    #     print("something went wrong") # prints if some else goes wrong
-    #     pass
 inputted.close()  # closing
 output.close()  # closing
