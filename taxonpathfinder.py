e0851e91a21d3bae598fe68d2899792ac8452e37
Your Name <adepennart@gmail.com>
2022-03-09 15:15:53 +0100

    comment taxonpathfinder.py file

--- taxonpathfinder.py
+++ taxonpathfinder.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Title: taxonpathparser.py
-Date: March 8th, 2022
+Date: March 9th, 2022
 Author: Auguste de Pennart
 
 Description:
@@ -15,18 +15,18 @@ List of functions:
     
     database_look_up:
         creates database as list
-        finds index of species of interest
+        finds index of taxa of interest
 
       lineage_look_up:
-          while loop through list to find parent ids of species of interest, all the way to root
+          while loop through list to find parent ids of taxa of interest, all the way to root
           meanwhile finds each id taxon name as you go up the lineage
 
 List of "non standard modules"
     No "non standard modules" are used in the program.
 
 Procedure:
-    1. get database and species of interest from input
-    2. find species of interest in database
+    1. get database and taxa of interest from input
+    2. find taxa of interest in database
     3. run through database as a list to find parent ids from which to find lineage
     4. print lineage to standard output
 
@@ -38,6 +38,7 @@ known error:
     2. can directly read database from online
     3. use argparse to determine whether input is taxid or taxon(right now just taxon)
     4. does not work with output file request
+    5. dictionaries should reject bad inputs
  """
 
 #import packages
@@ -46,7 +47,7 @@ import re
 import sys
 from pathlib import Path
 
-#insert input database here
+#input database
 inputfile = 'taxonomy.dat'
 #outputfile = 'homo_sapiens.json'
 
@@ -54,114 +55,210 @@ inputfile = 'taxonomy.dat'
 #----------------------------------------------------------------------------------------
 
 #input_pattern_generator
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#inputs:
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#input variables:
     #no inputs
 
-
 #use:
-    
+    #Takes user input as either taxa(word) or taxid(number) and creates a regex searchable 
+    #format for said input.
 
 #return:
+    #user_input:
+        # user input when prompted to input taxa or taxid
     #input_pattern:
-        #taxon or 
-    
+        #taxon or taxid in regex searchable format
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 def input_pattern_generator():
-    user_input=input("Input Species of Interest\n")
+    #get taxon or taxid from user
+    user_input=input("Input taxa or taxid of interest\n")
+    # checks whether user input is taxon or taxid
     try:
+        #checks if user input is an integer
         if int(user_input) :
+            #if user input is an integer creates input_id variable
             input_id = user_input
+            #creates id_flag
             id_flag=True
+    #if user input is not an integer the ValueError is raised
     except ValueError:
-        input_species= user_input
-        id_flag=False
-    #input_species[0]=input_species.capitalize()[0]
+        #if user input is not an integer creates input_taxa variable        
+        input_taxa= user_input
+    #checks whether id_flag was created
     if id_flag:
+        #if id_flag raised places input_id in input_pattern 
         input_pattern=f'^ID\s+:\s{input_id}$'
+    #checks whether id_flag was not created
     elif not id_flag:
-        input_pattern=f'SCIENTIFIC NAME\s+:\s{input_species}$'
-    return input_pattern, user_input
+        #if id_flag not raised places input_taxa in input_pattern
+        input_pattern=f'SCIENTIFIC NAME\s+:\s{input_taxa}$'
+    #function returns both user input and a regex searchable format of user input
+    return user_input, input_pattern
 
-#functions
+
+#database_look_up
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#input variables:
+    #database:
+        #taxonomic database of interest
+    #input_pattern:
+        #taxon or taxid user input in regex searchable format
+    #input_name:
+        #taxon or taxid user input
+        
+#use:
+    #Creates searchable database as form of list.
+    #Finds index/line in database where taxon or taxid is found.
+
+#return:
+    #taxonomy_list:
+        # taxonmic database as a list
+    #index:
+        # index/line where taxon or taxid is found
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 def database_look_up(database=None,input_pattern=None,input_name=None):
-    #assigned variables
-    taxonomy=[]
-    # opening inputfile
+    #empty taxonomy_list variable
+    taxonomy_list=[]
+    # opening databse file
     database = open(database, 'r')
-    #for loop through each line in database
+    #for loop through each enumerated line in database
     for n,line in enumerate(database):
-        #print(line)
-        taxonomy.append(line.strip())
-        # print(flag)
+        #print(line) #debugging
+        #adds line of database to taxonomy_list
+        taxonomy_list.append(line.strip())
+        # print(flag) #debugging
+        #searches for taxon or taxid in each line of database
         if re.search(input_pattern, line):
-            #test=re.search('(SCIENTIFIC NAME\s+:\s[A-Za-z\s\d\-\[\]]+)', line.strip()).group(1) 
+            #creates index variable, the line number for the taxon or taxid is on
             index=n
+            #prints to output that the taxon or taxid and its line number were found
             print(f"found {input_name} at line {index}")
-            # print(index)
-            #print(test)
+            # print(index) #debugging
         else:
             pass
+    #checks whether user defined taxon or taxid was found in database
     try:
+        #if index is a variable, taxon or taxid was found and the script can be continued
         index
+    #if index is not a variable raises UnboundLocalError flag
     except UnboundLocalError:
-        print("Species not found or mispelled, please try again")
+        #prints to standard output that taxon or taxid inputted was not found in database
+        print("taxa not found or mispelled, please try again")
+        #exits script
         exit()
     database.close()  # closing
-    return taxonomy, index
+    #function returns the taxonomic database as a list and line number for the taxon or taxid 
+    return taxonomy_list, index
+
+#lineage_find
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#input variables:
+    #database_input:
+        #indexable taxonomic database
+    #index:
+        #index/line for taxon or taxid in taxonomic database
+    #input_pattern:
+        #taxon or taxid user input in regex searchable format
+    #input_name:
+        #taxon or taxid user input
+        
+#use:
+    #while loop through each line above and below index/line in taxonomic dictionary
+    #to find PARENT ID line.
+    #Once PARENT ID is found, search for PARENT ID as the new ID, moving up lineage.
+    #Add SCIENTIFIC NAME of taxon to lineage list
+    #repeat steps, through while loop, till root is found
+
+
+#return:
+    #lineage:
+        #List of taxons from user inputted taxon or taxid to root of all life
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 def lineage_find(database_input=None, index=None,input_pattern=None,input_name=None):
     #assigned variables
+    #counter to move to next element in list
     count=0
+    #empty list to add taxa from taxa of interest to root
     lineage=[]
+    #while loop moving to each parent taxa
     while 1:
+        #while loop through each element found before taxon or taxid in taxonomic list 
         while 1:
+            #searches for // characters in list element, this defines the uppermost part of each taxon info section
             if '//' in database_input[index-count]:
-                # print(database_input[index-count])
-                break
-            #this needs to be updated for other organisms
+                # print(database_input[index-count])#debugging
+                #while loop broken if // characters found
+                break 
+            # searches for regex searchable format of user defined the taxon or taxid in list
             elif re.search(input_pattern, database_input[index-count]):
-                #this is repeated from the for loop through the database
-                # print(database_input[index-count])
+                # print(database_input[index-count])#debugging
+                # prints, again, to output that user defined taxon or taxid found at certain element number in list
                 print(f"confirmed find of {input_name} at line {index}")
+                # searches for SCIENTIFIC NAME in regex searchable format of user defined the taxon or taxid
                 if re.search('SCIENTIFIC NAME', input_pattern):
+                    # creates variable name, pulling out taxa name for the lineage list
                     name=re.search('SCIENTIFIC NAME\s+:\s([A-Za-z\s\d\-\[\]]+)$', database_input[index-count]).group(1)        
+                    #allows to proceed to next element in list
                     count+=1
-                    #not sure this will work for all searches
+                    #adds taxon to lineage list
                     lineage.append(f'{name}')
                 else:
-                    # print(database_input[index-count])
+                    # print(database_input[index-count]) #debugging
+                    #allows to proceed to next element in list
                     count+=1
-                
+            #searches for PARENT ID in list element, allowing to find parent taxon
             elif re.search('PARENT ID', database_input[index-count]):
-                # print(database_input[index-count])
+                # print(database_input[index-count]) #debugging
+                #creates variable for parent id for searching parent taxa
                 parent_id=database_input[index-count]
+                #allows to proceed to next element in list
                 count+=1
             else:
-                # print(database_input[index-count])
+                # print(database_input[index-count]) #debugging
+                #allows to proceed to next element in list
                 count+=1
+        #ensures index is not reobserved in while loop
         count=1
+        #while loop through each element found after taxon or taxid in taxonomic list 
         while 1:
+            #searches for // characters in list element, this defines the uppermost part of each taxon info section
             if '//' in database_input[index+count]:
-                # print(database_input[index+count])
+                # print(database_input[index+count]) #debugging
+                #while loop broken if // characters found
                 break
+            #searches for PARENT ID in list element, allowing to find parent taxon
             elif re.search('PARENT ID', database_input[index+count]):
-                # print(database_input[index+count])
+                # print(database_input[index+count]) #debugging
+                #creates variable for parent id for searching parent taxa
                 parent_id=database_input[index+count]
+                #allows to proceed to next element in list
                 count+=1
-            elif re.search('^GC ID\s+:\s[\d]+', database_input[index-count]): 
+            #searches for GC ID in list element, allowing to find taxon name 1-2 lines after
+            elif re.search('^GC ID\s+:\s[\d]+', database_input[index+count]):
+                #searches for MGC ID in list element, allowing to find taxon name 1 line after 
                 if re.search('^MGC ID\s+:\s[\d]+', database_input[index+count+1]): 
-                    # print(database_input[index-count])
+                    # print(database_input[index-count]) #debugging
+                    #creates variable of taxon name
                     name=re.search('SCIENTIFIC NAME\s+:\s([A-Za-z\s\d\-\[\]]+)', database_input[index+count+2]).group(1)  
+                    #adds taxon name to lineage list
                     lineage.append(name) 
+                    #allows to proceed to next element in list
                     count+=1
+                #searches for SCIENTIFIC NAME in list element, to find taxon name 
                 elif re.search('SCIENTIFIC NAME\s+:\s[A-Za-z\s\d\-\[\]]+$', database_input[index+count+1]):
-                    #this is repeated from the for loop through the database
-                    # print(database_input[index-count])
+                    # print(database_input[index-count]) #debuggigng
+                    #creates variable of taxon name
                     name=re.search('SCIENTIFIC NAME\s+:\s([A-Za-z\s\d\-\[\]]+)', database_input[index+count+1]).group(1)        
+                    #adds taxon name to lineage list
                     lineage.append(name) 
+                    #allows to proceed to next element in list
                     count+=1
                 else:
                     # print(database_input[index+count])
@@ -169,21 +266,32 @@ def lineage_find(database_input=None, index=None,input_pattern=None,input_name=N
             else:
                 # print(database_input[index+count])
                 count+=1
+        #reset counter for finding parent taxon name and grandparent taxon id
         count=0
+        #creates variable ID, to reset the parent id to the new child id 
         ID=re.search('^PARENT ID\s+:\s([\d]+)', parent_id).group(1)
-        # print(f'{ID} is the parent ID')
-        ID=f'ID                        : {ID}'
+        # print(f'{ID} is the parent ID') #debugging
+        #modifies ID varaible to be searchable in taxonomic list
+        ID=f'ID                        : {ID}' 
+        #checks whether new child id in taxonomic list
         try:
+            #creates new index for new child taxon
             index=database_input.index(ID)
+        #if new child id out of range of taxonomic list raises ValueError
         except ValueError:
+            #prints that the largest parent taxon has been found
             print("Reached root")
+            #breaks while loop
             break
+    #reverses lineage variable, so largest parent taxon shown first 
     lineage.reverse()  
+    #return the list of all taxon and all parent taxon to root
     return lineage
 
 #main code
-#___________________________________________________________#
+#--------------------------------------------------------------------------------------
 
+#input file verification
 # Here we are looking for 2 arguments (script, input file[1]) placed in the command line.
 # we are also making sure the input is valid
 if len(sys.argv) == 2 and Path(sys.argv[1]).is_file():
@@ -220,14 +328,29 @@ else:
         exit()
 
 
-#get searchable pattern
-input_pattern_generator_output=input_pattern_generator()
-#get index and database as lsit
-database_look_up_output=database_look_up(inputfile,input_pattern_generator_output[0],input_pattern_generator_output[1])
+#get regex searchable pattern for user inputted taxon or taxid
+taxon_and_pattern=input_pattern_generator()
 
-#find lineage
-lineage_find_output=lineage_find(database_look_up_output[0],database_look_up_output[1],input_pattern_generator_output[0],input_pattern_generator_output[1])
-print(lineage_find_output)
+#get database as list and taxon or taxid index
+list_and_index=database_look_up(inputfile,taxon_and_pattern[1],taxon_and_pattern[0])
+
+#find lineage of taxon or taxid 
+lineage=lineage_find(list_and_index[0],list_and_index[1],taxon_and_pattern[1],taxon_and_pattern[0])
+
+#graphical representation of lineage
+#assigned variables
+tab=""
+count=0
+#for loop through each taxa name
+for taxa in lineage:
+    #print to output
+    print(f'{tab}{taxa}')
+    if count==0:
+        tab+='+---'
+    tab='\t'+tab
+    count+=1
+    
+# print(lineage)
 
 
 #output.close()  # closing
