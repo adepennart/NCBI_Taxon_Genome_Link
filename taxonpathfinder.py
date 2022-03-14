77d9c962587ccfb480863fb4fd2c16e76596745a
Your Name <adepennart@gmail.com>
2022-03-14 15:42:27 +0100

    modify taxonpathfinder.py streamlined to new function species_find, lineage_visualisation and argparse ready

--- taxonpathfinder.py
+++ taxonpathfinder.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 """
 Title: taxonpathparser.py
-Date: March 13th, 2022
+Date: March 14th, 2022
 Author: Auguste de Pennart
 Description:
-    Finds taxon path from taxid or taxon
+    This program finds number of genome assemblies for user-specified taxid or taxon
+    
 List of functions:
     entrez_search:
 
@@ -16,6 +17,8 @@ List of functions:
     taxon_or_taxid:
 
     lineage_search:
+        
+    lineage_visualisation:
 
     assembled_genome_find:
 
@@ -23,29 +26,60 @@ List of functions:
 
 List of "non standard modules"
     No "non standard modules" are used in the program.
+    
 Procedure:
     1. get database and taxa of interest from input
     2. find taxa of interest in database
     3. run through database as a list to find parent ids from which to find lineage
     4. print lineage to standard output
+    
 Usage:
-    python taxonpathfinder.py
+    python taxonpathfinder.py [-h] [-v] USER_INPUT [USER_INPUT ...] 
+    
 known error:
-    2. lineage is obsolete right now, hopefully can get parent taxa not genome assembled
-    4. does not work with output file request
-    3. ask if environmental samples wanted
+    1. lineage_search and lineage_visualisation are obsolete right now, finding parent could be of use in program development
+    2. does not produce an output file
+    3. removes environmental samples from search, an argparse option could be available
+    4. only searches assemblies, searching other population genetics information could be of use
+    5. forloops through each generation, even if not needed
+
  """
 
 # import modules
 # ----------------------------------------------------------------------------------------
 import re  # module for using regex
-import sys  # module for using terminal
-from pathlib import Path  # module for verifying file path
+import argparse #module for terminal use
 from urllib.request import urlopen  # module to open the url
 from lxml import etree  # module to read xml files
+
+#argparse
 # ----------------------------------------------------------------------------------------
+#program description
+usage='This program finds number of genome assemblies for user-specified taxid or taxon'
+parser=argparse.ArgumentParser(description=usage)#create an argument parser
+req_arg= parser.add_argument_group(title="required arguments")
+#creates the argument for program version
+parser.add_argument('-v', '--version', 
+                    action='version', 
+                    version='%(prog)s 2.0')
+#creates the argument where email will be inputed
+req_arg.add_argument('-e', '--email',
+                    metavar='EMAIL',
+                    dest='email',
+                    required=True,
+                    help="email required to access NCBI's databases")
+#creates the argument where taxon or taxid will be inputed
+req_arg.add_argument('-i', '--input',
+                    metavar='USER_INPUT',
+                    dest='user_input',
+                    nargs='+',
+                    required=True,
+                    help='user-specified taxid or taxon')
+args=parser.parse_args()#parses command line
+    
 #functions
 # ----------------------------------------------------------------------------------------
+
 # entrez_search
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # input variables:
@@ -78,12 +112,6 @@ def entrez_search(user_input=None,database=None,next_of_kin=False):
     entrez_s_read = entrez_s.read()  # reads the url content
     # print(entrez_s)
     xml_s = etree.XML(entrez_s_read)  # parses the content into xml format
-    if xml_s is None:
-        # print('entrez_search returned empty')
-        pass
-    else:
-        # print('successful entrez_search')
-        pass
     return xml_s
 
 
@@ -105,7 +133,6 @@ def entrez_search(user_input=None,database=None,next_of_kin=False):
 def entrez_fetch(user_input=None,database=None):
     # assigned variables
     # base url for fetching lineage
-    print(user_input)
     baseurl_fetch = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?"
     #
     query = f"db={database}&id=[{user_input}]&format=xml&RetMax=100000"
@@ -113,12 +140,6 @@ def entrez_fetch(user_input=None,database=None):
     entrez = urlopen(url)  # opens the url with urlopen module
     entrez_read = entrez.read()  # reads the url content
     xml_f = etree.XML(entrez_read)  # parses the content into xml format
-    if xml_f is None:
-        # print('entrez_fetch returned empty')
-        pass
-    else:
-        # print('successful entrez_fetch')
-        pass
     return xml_f
 
 # data_find
@@ -171,8 +192,6 @@ def data_find(xml=None,pattern_search=None,tree_build=False,first_data=False):
         data_list = data_list[0]
         data_list = data_list.replace(" ", "%20")
         data_list = '\"' + data_list + '\"'
-    elif not first_data:
-        pass
     return data_list
 
 # taxon_or_taxid
@@ -190,24 +209,32 @@ def data_find(xml=None,pattern_search=None,tree_build=False,first_data=False):
     # ID:
         #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-def taxon_or_taxid(user_input=None):
+def taxon_or_taxid(species_ID=None):
+    #assigned variables
+    user_input=""
+    #changes datatype from list to string
+    for n, element in enumerate(species_ID):
+        if n != 0: 
+            user_input+=' ' + element
+        elif n == 0:
+            user_input=element
     # checks whether user input is taxon or taxid
     try:
         # checks if user input is an integer
-        if int(user_input):
+        if re.search('^[\d]+$', user_input):
             ID=user_input
             xml_f = entrez_fetch(user_input, 'taxonomy')
             species = data_find(xml_f, 'ScientificName',first_data=True)
-    # if user input is not an integer the ValueError is raised
-    except ValueError:
-        species=user_input
-        xml_s = entrez_search(user_input, 'taxonomy')
-        ID = data_find(xml_s, 'Id')
-    try:
+         # checks if user input is an integer
+        elif not re.search('^[\d]+$', user_input):
+            user_input = '\"'+ user_input.replace(" ", "%20") + '\"'
+            species=user_input
+            xml_s = entrez_search(user_input, 'taxonomy')
+            ID = data_find(xml_s, 'Id')
         test=species[0]+ ID[0]
     except IndexError:
         # prints to standard output that taxon or taxid inputted was not found in database
-        print("taxid not found in NCBI taxonomy database or misspelled, please try again")
+        print(f'''{user_input.replace("%20", " ").replace('"', "")} not found in NCBI taxonomy database or misspelled, please try again''')
         # exits script
         exit()
     # returns whether id_flag is True or False
@@ -237,6 +264,36 @@ def lineage_search(user_input=None):
     # return the list of all taxon and all parent taxon to root
     return lineage
 
+# lineage_visualisation
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# input variables:
+# no inputs
+
+# use:
+# graphical representation of lineage
+
+
+# return:
+# user_input:
+# user input when prompted to input taxa or taxid
+# input_pattern:
+# taxon or taxid in regex searchable format
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def lineage_visualisation(lineage=None):
+    # assigned variables
+    tab=""
+    count=0
+    #for loop through each taxa name
+    for taxa in lineage:
+        #print to output
+        print(f'{tab}{taxa}')
+        if count==0:
+            tab+='+---'
+        tab='\t'+tab
+        count+=1
+
+
 # assembled_genome_find
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # input variables:
@@ -255,9 +312,14 @@ def lineage_search(user_input=None):
 # if error shown in url, should escape
 
 def assembled_genome_find(user_input=None):
-    user_input=taxon_or_taxid(user_input)
-    xml_s=entrez_search(user_input[0],'assembly')
-    genomes=data_find(xml_s,'Id')
+    for species in user_input:
+        species=[species]
+        species_ID=taxon_or_taxid(species)
+        # print(species_ID)
+        # xml_s=entrez_search(species,'assembly')
+        xml_s=entrez_search(species_ID[0],'assembly')
+        genomes=data_find(xml_s,'Id')
+        print(f"""number of assembled genomes for {species_ID[0].replace('%20', ' ').replace('"', "")} is {len(genomes)}""")
     # return the list of all taxon and all parent taxon to root
     return len(genomes)
 
@@ -304,7 +366,7 @@ def child_find(user_input=None):
     taxon = data_find(xml_f, 'ScientificName',first_data=True)
     species_limit = data_find(xml_f, 'Rank',first_data=True)
     is_species[taxon] = species_limit
-    print(f'''{taxon.replace("%20", " ").replace('"', "")} is  of rank {species_limit.replace("%20", " ").replace('"', "")}''')
+    print(f'''{taxon.replace("%20", " ").replace('"', "")} is of rank {species_limit.replace("%20", " ").replace('"', "")}''')
     #maybe not needed
     if species_limit == 'species':
         count += 1
@@ -321,12 +383,12 @@ def child_find(user_input=None):
                 xml_f = entrez_fetch(ID, 'taxonomy')
                 taxon = data_find(xml_f, 'ScientificName',first_data=True)
                 species_limit = data_find(xml_f, 'Rank',first_data=True)
-                print(f'''{taxon.replace("%20", " ").replace('"', "")} is  of rank {species_limit.replace("%20", " ").replace('"', "")}''')
+                print(f'''{taxon.replace("%20", " ").replace('"', "")} is of rank {species_limit.replace("%20", " ").replace('"', "")}''')
                 child_list.append(taxon)
                 is_species[taxon]=species_limit
             #fix this too
-            if 'environmental%20samples' in child_list:
-                child_list.remove('environmental%20samples')
+            if '"environmental%20samples"' in child_list:
+                child_list.remove('"environmental%20samples"')
             # print(child_list)
             for ID in child_list:
                 # print(ID)
@@ -338,9 +400,6 @@ def child_find(user_input=None):
                 child_lineage[ID] = data_list
             if not grandchild_id_list:
                 count+=1
-                print("no next gen")
-            else:
-                print('next gen present')
             print(f'found {len(grandchild_id_list)} children taxon')
             child_id_list = grandchild_id_list
             # print(child_id_list)
@@ -353,62 +412,54 @@ def child_find(user_input=None):
                 break
     return generations, is_species
 
+# species_find
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# input variables:
+# database_input:
+# indexable taxonomic database
+# index:
+# index/line for taxon or taxid in taxonomic database
+# input_pattern:
+# taxon or taxid user input in regex searchable format
+# input_name:
+# taxon or taxid user input
+
+# use:
+# while loop through each line above and below index/line in taxonomic dictionary
+# to find PARENT ID line.
+# Once PARENT ID is found, search for PARENT ID as the new ID, moving up lineage.
+# Add SCIENTIFIC NAME of taxon to lineage list
+# repeat steps, through while loop, till root is found
+
+
+# return:
+# lineage:
+# List of taxons from user inputted taxon or taxid to root of all life
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def species_find(generations=None,is_species=None):
+    #assigned variables
+    species_list=[]
+    for generation, species in generations.items():
+        #could come up with a way to not forloop through each generation if not needed
+        for name, id in species.items():
+            # print(name)
+            if is_species[name]== '\"'+'species'+'\"':
+                species_list.append(name)
+    #print(species_list)
+    return species_list
+
+
 # main code
 # --------------------------------------------------------------------------------------
+## replaces white space with %20, so url can be searched
 
-# uses the files specified above in the script
-if len(sys.argv) == 1:
-    pass
-
-# exits script if unexpected arguments in commandline.
-else:
-    try:
-        raise ArithmeticError
-    except:
-        print("Looking for one argument, try again")
-        exit()
-
-email = 'adepennart@gmail.com'
-# email = input("Input email for NCBI database access\n")
-user_input = input("Input taxa or taxid of interest\n")
-# replaces white space with %20, so url can be searched
-user_input = user_input.replace(" ", "%20")
-user_input = '\"'+ user_input + '\"'
-
-# #get regex searchable pattern for user inputted taxon or taxid
-# lineage=lineage_search(user_input)
-#
-# # graphical representation of lineage
-# # assigned variables
-# tab=""
-# count=0
-# #for loop through each taxa name
-# for taxa in lineage:
-#     #print to output
-#     print(f'{tab}{taxa}')
-#     if count==0:
-#         tab+='+---'
-#     tab='\t'+tab
-#     count+=1
-
-#
-output = child_find(user_input)
-generations=output[0]
-is_species=output[1]
+output = child_find(args.user_input)
+# generations=output[0]
+# is_species=output[1]
 # print(is_species)
-# print(child[len(child)-1])
-for generation, species in generations.items():
-    #could coome up with a way to not forloop through each generation if not needed
-    for name, id in species.items():
-        # print(name)
-        if is_species[name]== '\"'+'species'+'\"':
-            genomes = assembled_genome_find(name)
-            name=name.replace('%20', ' ')
-            print(f'number of assembled genomes for {name} is {genomes}')
-        else:
-            pass
-
-
-#input species, ask if you cna input genus
-#while loop looking through each parent group until genome sequence found
+species_list=species_find(output[0],output[1])
+#
+genomes = assembled_genome_find(species_list)
+# print(f"""number of assembled genomes for {species.replace('%20', ' ').replace('"', "")} is {genomes}""")
 
